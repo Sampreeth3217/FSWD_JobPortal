40bd3aca2702e38680ffaefcd7a469045fa9082d
Sampreeth3217 <nschowdary1234@gmail.com>
2024-10-14 16:20:04 +0530

    push to production: changed cors to * and url of backend in constants

--- backend/index.js
+++ backend/index.js
@@ -17,14 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'*',
     credentials:true
 }
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
-
 
 // api's
 app.use("/api/v1/user", userRoute);
