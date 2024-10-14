14eb4275c0ea704da50c049003f7d92d0830e0d6
Sampreeth3217 <nschowdary1234@gmail.com>
2024-10-14 23:47:58 +0530

    changed the vercel.json file and also the endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
@@ -11,7 +10,7 @@ import applicationRoute from "./routes/application.route.js";
 dotenv.config({});
 
 const app = express();
-
+import cors from 'cors';
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
