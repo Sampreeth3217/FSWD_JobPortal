05887953305700c2db301d526536886f6219e93c
Sampreeth3217 <nschowdary1234@gmail.com>
2024-10-14 16:30:32 +0530

    changed cors policy to *

--- backend/index.js
+++ backend/index.js
@@ -16,14 +16,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'*',
-    credentials:true
-}
+// const corsOptions = {
+//     origin:'*',
+//     credentials:true
+// }
 
-app.use(cors(corsOptions));
+app.use(cors({origin:"*"}));
 
 const PORT = process.env.PORT || 3000;
+
 
 // api's
 app.use("/api/v1/user", userRoute);
